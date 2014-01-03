232a59ab90b4baf8f81c3564c7538726f3426954
DanSnow <dododavid006@gmail.com>
2014-01-03 14:59:52 +0800

    update

--- 401.php
+++ 401.php
@@ -1,9 +1,12 @@
 <div align="center">
   <table>
 <?php
+echo "<tr>\n";
 for($i=0;$i<5;$i++){
-echo "<tr><td><a href='image/401/401-0$i.JPG' data-lightbox='roadtrip'><img src='image/capture/401/401-0$i-s.jpg'></a></td></tr>";
+  echo "<td><a href='image/401/401-0$i.JPG' data-lightbox='roadtrip'><img src='image/capture/401/401-0$i-s.jpg'></a></td>\n";
+  if(($i+1)%2 == 0) echo "</tr>\n<tr>\n";
 }
+echo "</tr>\n";
 ?>
   </table>
 </div>
