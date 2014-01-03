182f3fc7cc6f420a1cab55a0340f6cff579222c0
DanSnow <dododavid006@gmail.com>
2014-01-03 14:39:11 +0800

    update

--- 401.php
+++ 401.php
@@ -1,8 +1,8 @@
-<div>
+<div style="center">
   <table>
 <?php
 for($i=0;$i<5;$i++){
-echo "<tr><td><a href='image/401/401-0$i.JPG' data-lightbox='roadtrip'><img src='image/capture/401/401-0$i-s.jpg'></a></td></tr>";
+echo "<tr><td align='center'><a href='image/401/401-0$i.JPG' data-lightbox='roadtrip'><img src='image/capture/401/401-0$i-s.jpg'></a></td></tr>";
 }
 ?>
   </table>
