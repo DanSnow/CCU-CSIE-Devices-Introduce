d1757d05f78c62e0ef350b467822c4a0fe71ca99
DanSnow <dododavid006@gmail.com>
2014-01-03 14:32:05 +0800

    update

--- 401.php
+++ 401.php
@@ -3,7 +3,7 @@
 <?php
 echo "<tr>";
 for($i=0;$i<5;$i++){
-  echo "<td><a href='image/401/401-0$i.jpg' data-lightbox='roadtrip'><img src='image/capture/401/401-0$i-s.jpg'></a></td>";
+  echo "<td><a href='image/401/401-0$i.JPG' data-lightbox='roadtrip'><img src='image/capture/401/401-0$i-s.jpg'></a></td>";
   if(($i+1) % 3 == 0) echo "</tr>\n<tr>";
 }
 echo "</tr>";
