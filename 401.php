cab608b90b4269d7803d60897efb1ff90bd9288a
DanSnow <dododavid006@gmail.com>
2014-01-03 14:28:58 +0800

    update

--- 401.php
+++ 401.php
@@ -3,7 +3,7 @@
 <?php
 echo "<tr>";
 for($i=0;$i<5;$i++){
-  echo "<td><a href='img/image-2.jpg' data-lightbox='roadtrip'><img src='image/capture/401/401-0$i-s.jpg'></a></td>";
+  echo "<td><a href='imgage/401/401-0$i.jpg' data-lightbox='roadtrip'><img src='image/capture/401/401-0$i-s.jpg'></a></td>";
   if(($i+1) % 3 == 0) echo "</tr>\n<tr>";
 }
 echo "</tr>";
--- index.php
+++ index.php
@@ -5,44 +5,41 @@
         <script src="js/jquery-1.10.2.min.js"></script>
         <script src="js/lightbox-2.6.min.js"></script>
         <link href="css/lightbox.css" rel="stylesheet" />
-//        <script src="js/jquery-2.0.3.min.js" ></script>
-//        <link href="js/shadowbox-3.0.3/shadowbox.css" rel="stylesheet" type="text/css" />
-//        <script src="js/shadowbox-3.0.3/shadowbox.js" ></script>
+        <script src="js/jquery-2.0.3.min.js" ></script>
         <style>
 
         </style>
         <script>
-            // function clickIE4(){
-            //     if (event.button==2){
-            //         return false;
-            //     }
-            // }
-            // function clickNS4(e){
-            //     if (document.layers||document.getElementById&&!document.all){
-            //         if (e.which==2||e.which==3){
-            //             return false;
-            //         }
-            //     }
-            // }
-            // function OnDeny(){
-            //     if(event.ctrlKey || event.keyCode==78 && event.ctrlKey || event.altKey || event.altKey && event.keyCode==115){
-            //         return false;
-            //     }
-            // }
-            // if (document.layers){
-            //     document.captureEvents(Event.MOUSEDOWN);
-            //     document.onmousedown=clickNS4;
-            //     document.onkeydown=OnDeny();
-            // }else if (document.all&&!document.getElementById){
-            // document.onmousedown=clickIE4;
-            // document.onkeydown=OnDeny();
-            // }
-            // document.oncontextmenu=new Function("return false");
-            //$(document).ready(function() {
-            //                  $("#mainFrame").click(function() {
-            //                    });
-            //});
-            //Shadowbox.init();
+            function clickIE4(){
+                if (event.button==2){
+                    return false;
+                }
+            }
+            function clickNS4(e){
+                if (document.layers||document.getElementById&&!document.all){
+                    if (e.which==2||e.which==3){
+                        return false;
+                    }
+                }
+            }
+            function OnDeny(){
+                if(event.ctrlKey || event.keyCode==78 && event.ctrlKey || event.altKey || event.altKey && event.keyCode==115){
+                    return false;
+                }
+            }
+            if (document.layers){
+                document.captureEvents(Event.MOUSEDOWN);
+                document.onmousedown=clickNS4;
+                document.onkeydown=OnDeny();
+            }else if (document.all&&!document.getElementById){
+            document.onmousedown=clickIE4;
+            document.onkeydown=OnDeny();
+            }
+            document.oncontextmenu=new Function("return false");
+            $(document).ready(function() {
+                             $("#mainFrame").click(function() {
+                               });
+            });
         </script>
     </head>
     <body onselectstart="return false">
